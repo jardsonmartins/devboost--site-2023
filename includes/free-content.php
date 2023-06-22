93d061fe0a0e7214389aec5ec27978b34bdc9f32
Jardson Martins <jardsonmartins12@gmail.com>
2023-06-22 19:57:58 -0300

    Update content-free

--- includes/free-content.php
+++ includes/free-content.php
@@ -2,7 +2,7 @@
     <h3>Conteúdo gratuito</h3>
     <div class="all">
         <a href="https://www.youtube.com/@dev_boost" target="_blank" class="card-free-content">
-            <img src="<?php echo get_template_directory_uri()?>/img/material-gratuito-devboost.png" alt="">
+            <img src="<?php echo get_template_directory_uri()?>/img/material-gratuito-devboost.jpg" alt="">
         </a>
     </div>
 </div>
